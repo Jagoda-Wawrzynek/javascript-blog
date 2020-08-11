7246877d600afeec38cdf63c4cfb38cb2ea07a2f
Jagoda Wawrzynek <jagoda.wawrzynek@gmail.com>
2020-08-11 19:26:18 +0200

    Modified code index.html

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@ for(let link of links){
 
   const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list',
 
 function generateTitleLinks(){
 
@@ -84,3 +85,35 @@ function generateTitleLinks(){
 
   };
 generateTitleLinks();
+
+/* adding tags to the article */
+
+function generateTags(){
+  /* [DONE] find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+    for (let article of articles) {
+  /* START LOOP: for every article: */
+      const tagsWrapper = article.querySelector(optArticleTagsSelector);
+      console.log(tagsWrapper);
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
